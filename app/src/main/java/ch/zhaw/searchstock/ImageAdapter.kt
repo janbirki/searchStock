d155851e7ce6357aee4c83907c41d7a50b8ee761
jb <jan.birkhofer@bluewin.ch>
2022-06-25 16:22:25 +0200

    add working request to get photo IDs and consider searchtext

---
+++ app/src/main/java/ch/zhaw/searchstock/ImageAdapter.kt
@@ -0,0 +1,46 @@
+package ch.zhaw.searchstock
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import ch.zhaw.searchstock.databinding.ImagesBoardBinding
+
+class ImageAdapter(var images: List<ImageEntry>, val context: Context) : BaseAdapter() {
+
+    var layoutInflater : LayoutInflater
+    private var _binding: ImagesBoardBinding? = null
+    private val binding get() = _binding!!
+    private var bindings = mutableMapOf<View,ImagesBoardBinding>()
+    init {
+        layoutInflater = LayoutInflater.from(context)
+    }
+
+    override fun getCount(): Int { //number of elements to display
+        return images.size}
+
+    override fun getItem(index: Int): ImageEntry { //item at index
+        return images.get(index)}
+
+    override fun getItemId(index: Int): Long { //itemId for index
+        return index.toLong()
+    }
+
+    override fun getView(index: Int, oldView: View?,
+                         viewGroup: ViewGroup?): View {
+
+        var view : View
+        if (oldView == null) { //check if we get a view to recycle
+            _binding = ImagesBoardBinding.inflate(layoutInflater, viewGroup, false)
+            view = binding.root;bindings[binding.root] = binding
+        } else { //if yes, use the oldview
+            view = oldView
+            _binding = bindings[view]
+        }
+        val image = getItem(index) //get the data for this index
+        binding.id.text = image.id
+
+        return view
+    }
+}
--- app/src/main/java/ch/zhaw/searchstock/StartFragment.kt
+++ app/src/main/java/ch/zhaw/searchstock/StartFragment.kt
@@ -1,16 +1,24 @@
 package ch.zhaw.searchstock
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import ch.zhaw.searchstock.databinding.FragmentFirstBinding
+import ch.zhaw.searchstock.databinding.FragmentStartBinding
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Klaxon
 
-class FirstFragment : Fragment() {
+class StartFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentStartBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,7 +29,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -29,7 +37,57 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+/*
+        val inputStream = requireContext().resources.openRawResource(R.raw.photos)
+        // convert
+        val results = Klaxon().parse<Results>(inputStream)
+        val adapter = ImageAdapter(results!!.results, requireContext())
+        binding.imagesList.adapter = adapter
+ */
 
+        // Beispiel query with key
+        //https://api.unsplash.com/search/photos?query=canada&client_id=hu_yKrF9g21PFsMUQLh7VMwcDoIgh9s_eBn4Szi_xsI
+
+        binding.buttonSearch.setOnClickListener {
+            //read search text in
+            val searchText = binding.searchInput.text
+            if (!searchText.isEmpty()) {
+                // create url
+                val urlSearch = "https://api.unsplash.com/search/photos?query=" + searchText + "&client_id=hu_yKrF9g21PFsMUQLh7VMwcDoIgh9s_eBn4Szi_xsI"
+
+                //create a request queue
+                val requestQueue = Volley.newRequestQueue(requireContext())
+                //define a request.
+                val request = StringRequest(
+                    Request.Method.GET, urlSearch,
+                    Response.Listener<String> { response ->
+                        val results = Klaxon().parse<Results>(response)
+                        val adapter = ImageAdapter(results!!.results, requireContext())
+                        binding.imagesList.adapter = adapter
+                    },
+                    Response.ErrorListener {
+                        //use the porvided VolleyError to display
+                        //an error message
+                    })
+                //add the call to the request queue
+                requestQueue.add(request)
+            }
+        }
+
+/*
+        val imageURL = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515"
+        var image: Bitmap? = null
+        try {
+            val `in` = java.net.URL(imageURL).openStream()
+            image = BitmapFactory.decodeStream(`in`)
+            binding.imageView.setImageBitmap(image)
+        }
+        catch (e: Exception) {
+            Log.e("Error Message", e.message.toString())
+            e.printStackTrace()
+        }
+
+ */
     }
 
     override fun onDestroyView() {
@@ -37,3 +95,7 @@ class FirstFragment : Fragment() {
         _binding = null
     }
 }
+
+class Results(val results: List<ImageEntry>)
+
+class ImageEntry(val id: String)
